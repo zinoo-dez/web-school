3aa92bc40bdaada5fb7b5909f9bb70f7f6af405a
zinoo-dez <zinoo.dev@gmail.com>
2025-03-03 13:01:18 +0630

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>hello world!</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum esse, alias quas sequi consequuntur sint, sed in doloremque cupiditate eos voluptas non rem, placeat culpa voluptatem ratione. Aliquam, iusto labore.</p>
+      <a href="#" className="underline hover:text-blue-600">Hello.</a>
     </div>
   );
 }
