4d313a4afdd13d4ede8d5428724946716e55b828
zinoo-dez <zinoo.dev@gmail.com>
2025-03-03 12:52:19 +0630

    test1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>hello world!</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum esse, alias quas sequi consequuntur sint, sed in doloremque cupiditate eos voluptas non rem, placeat culpa voluptatem ratione. Aliquam, iusto labore.</p>
     </div>
   );
 }
